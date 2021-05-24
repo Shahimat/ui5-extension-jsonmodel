1ec2bd8f80e5861d67795d5aea39eb2095358a29
Shahimat <ngs22071993@gmail.com>
2021-05-24 22:31:32 +0300

    chore: added base ui5 lib description

---
+++ src/libex/library.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    
+], function () {
+
+    sap.ui.getCore().initLibrary({
+        name: 'libex',
+        version: '0.0.1',
+        dependencies: [
+            'sap.ui.core'
+        ],
+        types: [],
+        interfaces: [],
+        controls: [
+            'libex.JSONModel'
+        ],
+        elements: []
+    });
+
+    return libex;
+
+}, /* bExport */ false);
