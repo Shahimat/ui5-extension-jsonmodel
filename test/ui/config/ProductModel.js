750c31693054816ab9bfe2ec5112bcb9f2ee6d3a
Shahimat <ngs22071993@gmail.com>
2021-05-27 18:19:01 +0300

    test[ui]: for base feat (fetch)

---
+++ test/ui/config/ProductModel.js
@@ -0,0 +1,75 @@
+sap.ui.define([
+  'libex/CRUDfetch'
+], (
+  CRUDfetch
+) => {
+  'use strict';
+
+  return [
+
+    new CRUDfetch('sample1', 'http://localhost:8090', [
+      {
+        type: 'create',
+        pattern: /^\/$/,
+        uri: (oContext) => `${oContext.url}/`,
+        body: (oContext) => JSON.stringify(oContext.params),
+        req: (oContext) => {
+          return {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8'
+            },
+          }
+        },
+        task: (oContext) => {}
+      },
+      {
+        type: 'read',
+        pattern: /^\/$/,
+        uri: (oContext) => `${oContext.url}/`,
+        body: (oContext) => {},
+        req: (oContext) => {},
+        task: (oContext) => {
+          oContext.model.setProperty(oContext.uri, oContext.oData);
+        }
+      },
+      {
+        type: 'read',
+        pattern: /^\/(\d+)$/,
+        uri: (oContext) => `${oContext.url}/${oContext.patternMatched[1]}`,
+        body: (oContext) => {},
+        req: (oContext) => {},
+        task: (oContext) => {}
+      },
+      {
+        type: 'update',
+        pattern: /^\/$/,
+        uri: (oContext) => `${oContext.url}/`,
+        body: (oContext) => JSON.stringify(oContext.params),
+        req: (oContext) => {
+          return {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8'
+            },
+          }
+        },
+        task: (oContext) => {}
+      },
+      {
+        type: 'delete',
+        pattern: /^\/(\d+)$/,
+        uri: (oContext) => `${oContext.url}/${oContext.patternMatched[1]}`,
+        body: (oContext) => {},
+        req: (oContext) => {
+          return {
+            method: 'DELETE',
+          }
+        },
+        task: (oContext) => {}
+      },
+    ]),
+
+  ];
+
+});
--- test/ui/controller/App.controller.js
+++ test/ui/controller/App.controller.js
@@ -1,41 +1,80 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    'sap/ui/model/Sorter'
- ], function (Controller, MessageToast, Sorter) {
+    'sap/ui/model/Sorter',
+    'libex/JSONModel',
+    'sap/ui/core/BusyIndicator',
+ ], function (Controller, MessageToast, Sorter, ExJSONModel, BusyIndicator) {
     'use strict';
     return Controller.extend('webapp.controller.App', {
 
         productSelectedItem: null,
 
         onInit: function () {
-            
+            this.model = this.getView().getModel('product');
+            this.onUpdateFrontendModelFromBack();
         },
 
         onSelectionChange: function (oEvent) {
             if (!oEvent) {
                 this.productSelectedItem = null;
+                this.byId('productTable').removeSelections();
                 return;
             }
             let aItemCells = oEvent.getParameter('listItem').getCells();
             this.productSelectedItem = Number(aItemCells[0].getValue());
         },
+
+        onUpdateFrontendModelFromBack: function () {
+            BusyIndicator.show(0);
+            return this.model.read('/').then(oData => {
+                BusyIndicator.hide();
+                return this._nextPromiseChain();
+            })
+        },
         
         onCreate: function () {
-
+            this.model.create('/', {
+                ProductName: 'some',
+                UnitPrice: 100
+            }).then(() => {
+                this.onUpdateFrontendModelFromBack();
+            })
         },
         
         onChange: function () {
-
+            if (this.productSelectedItem === null) {
+                MessageToast.show('Ничего не выбрано');
+                return;
+            }
+            this.model.read(`/${this.productSelectedItem}`).then((oData) => {
+                oData.UnitPrice = oData.UnitPrice + 156;
+                return this.model.update('/', oData);
+            }).then(() => {
+                this.onUpdateFrontendModelFromBack();
+            })
         },
         
         onDelete: function () {
-
+            if (this.productSelectedItem === null) {
+                MessageToast.show('Ничего не выбрано');
+                return;
+            }
+            this.model.delete(`/${this.productSelectedItem}`).then(() => {
+                this.onSelectionChange();
+                this.onUpdateFrontendModelFromBack();
+            })
         },
         
         onSort: function () {
 
-        }
+        },
 
-     });
- });
+        _nextPromiseChain: function (...args) {
+            return new Promise((res, rej) => {
+                res(...args);
+            });
+        },
+
+    });
+});
